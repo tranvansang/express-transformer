94037e9d6650ed927ac132d2a12f1b4cf8831ce8
Tran Sang <info@transang.me>
2020-02-16 14:19:00 +0700

    transform toint

--- src/plugins/toInt.test.ts
+++ src/plugins/toInt.test.ts
@@ -8,53 +8,34 @@ import flipPromise from 'flip-promise'
 describe('Transform', () => {
 	test('should check to int', async () => {
 		const req: {body: {key: string | number}} = {body: {key: '1'}}
-		await combineToAsync(
-			transformer('key').toInt(),
-			validateTransformation
-		)(req as Request, undefined as unknown as Response)
-		expect(req.body.key).toBe(1)
-
-		;(transformationResult(req as Request) as Array<any>).splice(0, 1)
-		req.body.key = 12
-		await combineToAsync(
-			transformer('key').toInt(),
-			validateTransformation
-		)(req as Request, undefined as unknown as Response)
-		expect(req.body.key).toBe(12)
-
-		;(transformationResult(req as Request) as Array<any>).splice(0, 1)
-		req.body.key = '1.2'
-		await combineToAsync(
-			transformer('key').toInt(),
-			validateTransformation
-		)(req as Request, undefined as unknown as Response)
-		expect(req.body.key).toBe(1)
-
-		;(transformationResult(req as Request) as Array<any>).splice(0, 1)
-		req.body.key = '1'
-		await flipPromise(combineToAsync(
-			transformer('key').toInt({min: 3}),
-			validateTransformation
-		)(req as Request, undefined as unknown as Response))
-		expect(req.body.key).toBe('1')
-
-		;(transformationResult(req as Request) as Array<any>).splice(0, 1)
-		req.body.key = '1'
-		await flipPromise(combineToAsync(
-			transformer('key').toInt({max: 0}),
-			validateTransformation
-		)(req as Request, undefined as unknown as Response))
-		expect(req.body.key).toBe('1')
-
-		;(transformationResult(req as Request) as Array<any>).splice(0, 1)
-		req.body.key = 'abc'
-		await flipPromise(combineToAsync(
-			transformer('key').toInt({max: 0}),
-			validateTransformation
-		)(req as Request, undefined as unknown as Response))
-		expect(req.body.key).toBe('abc')
-
-		;(transformationResult(req as Request) as Array<any>).splice(0, 1)
+		for (const [inp, out, opts] of [
+			['1', 1],
+			[12, 12],
+			['12', 12],
+			[12.02, 12],
+			[1.2, 1],
+			[-1.3, -1],
+		]) {
+			req.body.key = inp
+			await combineToAsync(
+					transformer('key').toInt(opts),
+					validateTransformation
+			)(req as Request, undefined as unknown as Response)
+			expect(req.body.key).toBe(out)
+		}
+		for (const [inp, opts] of [
+			['1', {min: 3}],
+			['1', {max: 0}],
+			['abc', {max: 0}]
+		]) {
+			req.body.key = inp
+			await flipPromise(combineToAsync(
+					transformer('key').toInt(opts),
+					validateTransformation
+			)(req as Request, undefined as unknown as Response))
+			expect(req.body.key).toBe(inp)
+			;(transformationResult(req as Request) as Array<any>).splice(0, 1)
+		}
 		delete req.body.key
 		await flipPromise(combineToAsync(
 			transformer('key').toInt({force: true}),
--- src/plugins/toInt.ts
+++ src/plugins/toInt.ts
@@ -10,7 +10,7 @@ declare module '../transformer' {
 export default (middleware: Middleware<string | number, number>) => {
 	middleware.toInt = ({min, max, ...transformOption}: { min?: number, max?: number } & ITransformOption = {}) =>
 		middleware.each((value: string | number, {path}) => {
-				value = typeof value === 'string' ? parseInt(value) : value
+				value = typeof value === 'number' ? Math.trunc(value) : parseInt(value)
 				if (isNaN(value) || !isFinite(value)) throw new TransformationError(`${path} must be an integer`)
 				if (min !== undefined && value < min) throw new TransformationError(`${path} must be at least ${min}`)
 				if (max !== undefined && value > max) throw new TransformationError(`${path} must be at most ${max}`)
