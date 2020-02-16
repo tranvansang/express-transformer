567b9c059556d03b78aadb979e0afaad3c0c76f6
Tran Sang <info@transang.me>
2020-02-16 13:06:12 +0700

    make instanceof work

--- src/TransformationError.ts
+++ src/TransformationError.ts
@@ -2,6 +2,8 @@ export const transformationErrorName = 'TransformationError'
 export default class TransformationError extends Error {
 	constructor(message?: string) {
 		super(message)
+		this.constructor = TransformationError
+		this.__proto__ = TransformationError.prototype
 		this.name = transformationErrorName
 	}
 }
