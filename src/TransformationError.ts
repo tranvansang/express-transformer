a05580cba201555542dee7c3f535ec250f8e0632
Tran Sang <info@transang.me>
2020-02-16 13:34:55 +0700

    use setprototypeof

--- src/TransformationError.ts
+++ src/TransformationError.ts
@@ -1,10 +1,8 @@
 export const transformationErrorName = 'TransformationError'
 export default class TransformationError extends Error {
-	__proto__: typeof TransformationError.prototype
 	constructor(message?: string) {
 		super(message)
-		this.constructor = TransformationError
-		this.__proto__ = TransformationError.prototype
 		this.name = transformationErrorName
+		Object.setPrototypeOf(this, TransformationError.prototype)
 	}
 }
